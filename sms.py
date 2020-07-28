f518299eff1be73ef67e0d340796ab8aac3f98bb
Moustafa Eid <54959148+Moustafa-Eid@users.noreply.github.com>
2020-07-28 17:37:49 -0400

    Update sms.py

--- sms.py
+++ sms.py
@@ -3,7 +3,7 @@ def sendSms(phone,message):
 	resp = requests.post('https://textbelt.com/text', {
   	'phone': phone,
   	'message': message,
-  	'key': '198a5e37ed6c9547ae327c2bef4078822d56b8e6IQVmLhgeVPjcdgI6JkA9H7Bnd',
+  	'key': 'Enter Code',
   	})
 
 #print(resp.json())
